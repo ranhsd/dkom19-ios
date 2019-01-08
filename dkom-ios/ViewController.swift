ecc09b868883e0e031a59bc1f4f0f2ca76df117d
Ran Hassid <ran.hassid@gmail.com>
2019-01-08 17:22:15 +0200

    Initial Commit

---
+++ dkom-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dkom-ios
+//
+//  Created by Ran Hassid on 08/01/2019.
+//  Copyright © 2019 Trench Girls LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
